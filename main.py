61f140fd65aae4f4cef462333872c804d90f7ac2
ekiann <annaekimova02@yandex.ru>
2024-04-04 12:26:49 +0300

    Login frame(kivy) part Two not work absolutely

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.core.window import Window
 from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
 
-Start = Builder.load_file("designLogin.kv")
 
+class Start(Screen):
+    pass
+
+lblKVLang = Builder.load_file("main.kv")
 
 class KVLangTestApp(App):
 
     def build(self):
-        Window.clearcolor = (2/255,	49/255,	93/255, 1)
-        Window.size = (360, 800)
-        return Start
+        # Window.clearcolor = (2/255,	49/255,	93/255, 1)
+        # Window.size = (360, 800)
+        return Start()
 
 
 if __name__ == '__main__':
