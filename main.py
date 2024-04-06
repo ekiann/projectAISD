5fb157da5f2f2fc36f831214d0379ddbb1ca8963
ekiann <annaekimova02@yandex.ru>
2024-04-06 21:24:02 +0300

    Login frame(kivy) part Five canva

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
 
 lblKVLang = Builder.load_file("main.kv")
 
@@ -16,6 +17,8 @@ class MyApp(App):
     def build(self):
         Window.clearcolor = (2/255, 49/255, 93/255, 1)
         Window.size = (360, 600)
+        self.width = Window.width
+        self.height = Window.height
         return NewClass()
 
 
